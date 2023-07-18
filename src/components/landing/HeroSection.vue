9525c3e9643791e6c1b613c589981139fcfff5a1
geeko <george.kvrivi@gmail.com>
2023-07-18 13:36:12 +0400

    fix: display correct elements on mobile header

--- src/components/landing/HeroSection.vue
+++ src/components/landing/HeroSection.vue
@@ -5,16 +5,10 @@ const emit = defineEmits(['scrollTo'])
 </script>
 
 <template>
-  <section
-    class="w-full flex flex-col justify-center items-center gap-8 md:gap-6 pt-32 xl:h-[75vh] pb-24"
-  >
-    <p
-      class="text-offGold text-2xl font-bold md:text-5xl 2xl:text-6xl w-[70%] 2xl:w-[38%] text-center"
-    >
+  <section class="w-full flex flex-col justify-center items-center gap-8 md:gap-6 pt-32 xl:h-[75vh] pb-24">
+    <p class="text-offGold text-2xl font-bold md:text-5xl 2xl:text-6xl w-[70%] 2xl:w-[38%] text-center">
       {{ $t('landing.hero_heading') }}
     </p>
-    <Button @click="emit('scrollTo')" class="px-[14px] py-2 md:px-4">{{
-      $t('landing.hero_button')
-    }}</Button>
+    <Button @click="emit('scrollTo')" class="px-[14px] py-2 md:px-4">{{ $t('landing.hero_button') }}</Button>
   </section>
 </template>
--- src/components/shared/header/AppHeader.vue
+++ src/components/shared/header/AppHeader.vue
@@ -41,6 +41,8 @@ provide('showNotifications', showNotifications)
 const router = useRouter()
 const currentRoute = computed(() => router.currentRoute.value.path)
 
+const canDisplayNavigationMenu = computed(() => !currentRoute.value === '/' && !currentRoute.value.startsWith('/auth'))
+
 const handleLogout = async () => {
   try {
     await request.post('/api/logout')
@@ -57,12 +59,13 @@ const newNotificationsCount = computed(() => useNotificationStore().newNotificat
   <header
     class="fixed select-none top-0 z-50 w-full bg-transparent h-[86px] flex justify-between items-center py-6 px-4 md:px-[40px] xl:px-[70px]"
   >
-    <h3 class="hidden md:block text-sm text-offGold font-medium md:text-base">MOVIE QUOTES</h3>
+    <h3 class="text-sm text-offGold font-medium md:text-base" v-if="!currentRoute.startsWith('/auth')">MOVIE QUOTES</h3>
     <HamburgerMenu
       class="cursor-pointer md:hidden text-white text-2xl"
+      v-if="canDisplayNavigationMenu"
       @click="modalStore.setShowMobileNavigation(true)"
     />
-    <div class="flex items-center gap-3">
+    <div class="flex items-center gap-3 flex-row-reverse md:flex-row">
       <SearchIcon
         class="lg:hidden cursor-pointer mr-5 h-6 w-6"
         color="white"
@@ -76,15 +79,15 @@ const newNotificationsCount = computed(() => useNotificationStore().newNotificat
       />
       <LanguageSwitcher />
       <SignUpButton
-        class="px-6 py-2 hidden md:flex"
+        class="px-4 md:px-6 text-sm md:text-base"
         @click="() => router.push({ name: 'register' })"
-        v-if="!userStore.isLoggedIn"
+        v-if="!userStore.isLoggedIn && !currentRoute.startsWith('/auth')"
         >{{ $t('landing.signup') }}
       </SignUpButton>
       <button
         @click="() => router.push({ name: 'login' })"
         v-if="!userStore.isLoggedIn && !currentRoute.startsWith('/auth')"
-        class="px-4 py-1.5 md:px-6 md:py-[6px] border border-white flex justify-center items-center text-white rounded hover:text-darkBlue hover:bg-white transition-colors"
+        class="px-4 py-1.5 text-sm m:text-base md:px-6 md:py-[8px] border border-white flex justify-center items-center text-white rounded hover:text-darkBlue hover:bg-white transition-colors"
       >
         {{ $t('landing.login') }}
       </button>
